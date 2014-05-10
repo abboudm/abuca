8041e25871c9f38547dac622c2ab361371f5410f
Matt DeLuca <mattdeluca64@gmail.com>
2014-05-09 19:17:55 -0700

    added example source files

--- src/com/badlogic/androidgames/AndroidBasicsStarter.java
+++ src/com/badlogic/androidgames/AndroidBasicsStarter.java
@@ -7,25 +7,31 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-
 public class AndroidBasicsStarter extends ListActivity {
-	String tests[] = {"LifeCycleTest" };
-	
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tests));
-		
-	}
-	@Override
-	protected void onListItemClick(ListView list, View view, int position, long id){
-		super.onListItemClick(list,  view,  position,  id);;
-		String testName = tests[position];
-		try {
-			Class clazz = Class.forName("com.badlogic.androidgames."+ testName);
-			Intent intent = new Intent(this, clazz);
-			startActivity(intent);
-		} catch (ClassNotFoundException e){
-			e.printStackTrace();
-		}
-	}
+    String tests[] = { "LifeCycleTest", "SingleTouchTest", "MultiTouchTest",
+            "KeyTest", "AccelerometerTest", "AssetsTest",
+            "ExternalStorageTest", "SoundPoolTest", "MediaPlayerTest",
+            "FullScreenTest", "WakeLockTest", "RenderViewTest", "ShapeTest", "BitmapTest",
+            "FontTest", "SurfaceViewTest" };
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, tests));
+    }
+
+    @Override
+    protected void onListItemClick(ListView list, View view, int position,
+            long id) {
+        super.onListItemClick(list, view, position, id);
+        String testName = tests[position];
+        try {
+            Class clazz = Class
+                    .forName("com.badlogic.androidgames." + testName);
+            Intent intent = new Intent(this, clazz);
+            startActivity(intent);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
